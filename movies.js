760ba37f1fd421a6e487a06f5174742217d31420
Bianca Reusch <reusch.bianca@gmail.com>
2020-08-27 09:25:32 -0500

    added movies to array

---
+++ movies.js
@@ -0,0 +1,21 @@
+let movies = [
+    { "title" : "Batman Begins",
+    "release_date": "Friday, August 7, 2020",
+    "runtime": 140,
+    "rating": "PG-13",
+    "genre": "Action"
+},
+    { "title": "Unhinged",
+        "release_date": "Friday, August 21, 2020",
+        "runtime" : 90,
+        "rating" : "R",
+        "genre": "Thriller"
+    },
+    { "title": "The Goonies",
+        "release_date": "Friday, November 15, 2019",
+        "runtime" : 114,
+        "rating": "PG",
+        "genre": "Action"
+    },
+
+];
